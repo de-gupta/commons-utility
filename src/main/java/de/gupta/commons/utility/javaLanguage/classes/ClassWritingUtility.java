0b7d2ae4de186ecda35e185da69ac13bdd92eda3
de-gupta <de-gupta>
2025-07-27 00:54:07 +0200

    class writing overload
    Took 55 minutes

--- src/main/java/de/gupta/commons/utility/javaLanguage/classes/ClassWritingUtility.java
+++ src/main/java/de/gupta/commons/utility/javaLanguage/classes/ClassWritingUtility.java
@@ -4,10 +4,24 @@ import de.gupta.commons.utility.io.path.PathUtility;
 import de.gupta.commons.utility.io.write.FileWritingUtility;
 import de.gupta.commons.utility.javaLanguage.packages.PackageExtractor;
 
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Optional;
 
 public final class ClassWritingUtility
 {
+	public static void writeClass(final String fileName, final String classContent, final Path contentRootPath,
+								  boolean overwrite)
+	{
+		Optional.ofNullable(contentRootPath)
+				.ifPresentOrElse(p -> writeClass(fileName, classContent, p.toString(), overwrite),
+						() ->
+						{
+							throw new IllegalArgumentException("Content root path cannot be null");
+						}
+				);
+	}
+
 	public static void writeClass(final String fileName, final String classContent, final String contentRootPath,
 								  boolean overwrite)
 	{
