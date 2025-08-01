346d208902ae011c9f6e387edd19ca667384e517
de-gupta <de-gupta>
2025-08-01 13:27:53 +0200

    added aletheia to dep and changed class writing utility
    Took 16 minutes

--- src/main/java/de/gupta/commons/utility/javaLanguage/classes/ClassWritingUtility.java
+++ src/main/java/de/gupta/commons/utility/javaLanguage/classes/ClassWritingUtility.java
@@ -1,11 +1,11 @@
 package de.gupta.commons.utility.javaLanguage.classes;
 
-import de.gupta.commons.utility.io.path.PathUtility;
+import de.gupta.aletheia.functional.Unfolding;
 import de.gupta.commons.utility.io.write.FileWritingUtility;
 import de.gupta.commons.utility.javaLanguage.packages.PackageExtractor;
 
 import java.nio.file.Path;
-import java.util.List;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 public final class ClassWritingUtility
@@ -25,9 +25,12 @@ public final class ClassWritingUtility
 	public static void writeClass(final String fileName, final String classContent, final String contentRootPath,
 								  boolean overwrite)
 	{
-		var path = PathUtility.fullPath(List.of(contentRootPath, PackageExtractor.extractPackageName(classContent)),
-				fileName);
-		FileWritingUtility.writeFileAndCreateDirectory(path, classContent, overwrite);
+		Unfolding.of(classContent)
+				 .metamorphose(PackageExtractor::extractPackageName)
+				 .metamorphose(p -> p.split("\\."))
+				 .metamorphose(segments -> Paths.get(contentRootPath, segments))
+				 .metamorphose(packagePath -> packagePath.resolve(fileName))
+				 .unlace(path -> FileWritingUtility.writeFileAndCreateDirectory(path, classContent, overwrite));
 	}
 
 	private ClassWritingUtility()
